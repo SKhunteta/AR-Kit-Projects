ad08c7f03fc1460a07b791eaa14bb6296e91855a
SKhunteta <shreyans.khunteta@gmail.com>
2019-10-08 16:25:33 -0700

    attached video

--- SceneKitProject/SceneKitProject/ViewController.swift
+++ SceneKitProject/SceneKitProject/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene(named: "art.scnassets/toy_robot_vintage.usdz")!
         
         // Set the scene to the view
         sceneView.scene = scene
