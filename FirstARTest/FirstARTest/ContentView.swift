443dd6a85909c7f33809f697f5355d32c44e1069
SKhunteta <shreyans.khunteta@gmail.com>
2019-10-07 22:54:25 -0700

    Tried to do lights

--- FirstARTest/FirstARTest/ContentView.swift
+++ FirstARTest/FirstARTest/ContentView.swift
@@ -24,12 +24,23 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
+        //let config = ARWorldTrackingConfiguration()
+        
+        //let spotLight = arView.environment.lighting
+    
+        
         var anchor:Experience.Box!
+        
         
         anchor = try! Experience.loadBox()
         anchor.generateCollisionShapes(recursive: true)
         
+       // arView.session.run(config, options: [])
         arView.scene.anchors.append(anchor)
+        //arView.scene.anchors.append(spotLight)
+        
+        
+        
         
         anchor.scale = SIMD3<Float>(0.001, 0.001, 0.01)
         
